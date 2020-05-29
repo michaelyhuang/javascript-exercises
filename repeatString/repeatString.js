6b8f58178d7edaa311e2bc6d6e8f93a6fa5a0508
michaelyhuang <michaelh2016@gmail.com>
2020-05-29 13:54:25 -0500

    complete helloWorld and repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(str, i) {
+    let result = '';
+    if (i < 0){
+        return('ERROR')
+    } else{
+        while (i > 0) {
+            result = result + str;
+            i--;
+        }
+    }
+    return(result)
 }
 
 module.exports = repeatString
