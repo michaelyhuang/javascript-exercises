fc4d5edc5d9d80ecb013e1b5d790a0a8f8b4feb5
michaelyhuang <michaelh2016@gmail.com>
2020-05-29 16:26:05 -0500

    solve reverseString and removeFromArray challenges

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,29 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    let args = Array.prototype.slice.call(arguments, 1);
+    let i = 0;
+    while (i < args.length){
+        let element = args[i];
+        arr = removeAllElements(arr, element);
+        i++;
+    }
+   return arr;
+}
 
+const removeOneElement = function(arr, el){
+    let index = arr.indexOf(el);
+    arr.splice(index, 1);
+    return arr;
+}
+
+const removeAllElements = function(arr,el){
+    if (arr.includes(el)){
+        while (arr.includes(el)){
+            arr = removeOneElement(arr,el);
+            return arr;
+        } 
+    } else {
+        return arr;
+    }
 }
 
 module.exports = removeFromArray
